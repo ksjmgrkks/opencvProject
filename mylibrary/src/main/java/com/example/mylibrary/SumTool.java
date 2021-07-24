21bfceadb8b227e6f491c7174f62b1a25ad15cf2
ksk <ksjmgrkks@naver.com>
2021-07-24 16:39:24 +0900

    aar 파일 생성 연습 (2021/07/24)

---
+++ mylibrary/src/main/java/com/example/mylibrary/SumTool.java
@@ -0,0 +1,14 @@
+package com.example.mylibrary;
+
+public class SumTool {
+    // 구현
+    // 생성자
+    public SumTool(){
+
+    }
+
+    //메소드
+    public int SumInt(int valFront, int valBack){
+        return valFront+valBack;
+    }
+}
